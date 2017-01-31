3067104b224a396b8c910c38b8f567172abc7103
Mike Martin <mikemartin604@gmail.com>
2017-01-31 15:04:20 -0330

    Calculate shipping

--- _add-ons/bison_shippo_shipping/api.bison_shippo_shipping.php
+++ _add-ons/bison_shippo_shipping/api.bison_shippo_shipping.php
@@ -3,20 +3,27 @@ require_once('vendor/autoload.php');
 
 class API_bison_shippo_shipping extends API {
 	public function calculateShipping()
-	{
-		Shippo::setApiKey($this->fetchConfig('api_key'));
-
-		$shipping_options = $this->core->getRates();
+  {
 		$customer = $this->addon->api('bison')->getCustomerInfo();
 
-		// This would be the index of the rate selected by the user
-		$selected_rate_index = 1;
+		$shipping = 0;
 
-		// After the user has selected a rate, use the corresponding object_id
-		$selected_rate = $customer['shipping_option'];
-		$shipping_options = $this->core->getRates();
-		$selected_rate_price = $shipping_options[$selected_rate]['price'];
+		if ($customer['shipping_option'] != '') {
+			Shippo::setApiKey($this->fetchConfig('api_key'));
 
-		return (float)$selected_rate_price*100;
-	}
+	    $shipping_options = $this->core->getRates();
+
+
+			// This would be the index of the rate selected by the user
+			$selected_rate_index = 1;
+
+			// After the user has selected a rate, use the corresponding object_id
+			$selected_rate = $customer['shipping_option'];
+			$shipping_options = $this->core->getRates();
+
+			$shipping = $shipping_options[$selected_rate]['price']*100;
+		}
+
+		return $shipping;
+  }
 }
