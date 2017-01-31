be5e56e99c0a8adb807be39d4ee1b7687affb1a7
Mike Martin <mikemartin604@gmail.com>
2017-01-31 15:07:35 -0330

    Retrieve and cache shipping rates

--- _add-ons/bison_shippo_shipping/core.bison_shippo_shipping.php
+++ _add-ons/bison_shippo_shipping/core.bison_shippo_shipping.php
@@ -3,87 +3,41 @@ require_once('vendor/autoload.php');
 
 class Core_bison_shippo_shipping extends Core
 {
-	/**
-	* Get Rates
-	*
-	* Retrieve cached rates or get rates from API
-	*
-	* @return array
-	**/
-	public function getRates() {
-		// if ($this->cache->exists($rates)) {
-		// 		return $this->cache->get($rates);
-		// }
+		/**
+		* Get Rates
+		*
+		* Retrieve cached rates or get rates from API
+		*
+		* @return array
+		**/
+		public function getRates() {
+			$customer = $this->addon->api('bison')->getCustomerInfo();
+			$rates_id = $customer['shipping_zip'] . date("Ym");
 
-		$rates = $this->getRatesFromAPI();
-		//$this->cache->put($rates);
+			// check cache to see if we've found this one before
+			if ($this->cache->exists($rates_id)) {
+				$rates = $this->cache->getYAML($rates_id);
+			} else {
+				$rates = $this->getRatesFromAPI();
+				// cache it
+				$this->cache->putYAML($rates_id, $rates);
+			}
 
-		return $rates;
-	}
-
-	/**
-	* Get Rates from API
-	*
-	* Retrieve all available shipping rates from Shippo API
-	*
-	* @return array
-	**/
-	public function getRatesFromAPI()
-	{
-		Shippo::setApiKey($this->fetchConfig('api_key'));
-
-		$bison_config = $this->addon->api('bison')->getBisonConfig();
-		$cart_items = $this->addon->api('bison')->getCartItems();
-		$total_weight = 0;
-		$customer = $this->addon->api('bison')->getCustomerInfo();
-
-		foreach ($cart_items as $item) {
-			$total_weight += $item[$bison_config['weight_field']] * $item['quantity'];
+			// all the region details (from the config file)
+			return $rates;
 		}
 
-		$from_address = array(
-			'object_purpose' => 'PURCHASE',
-			'name' => $this->config['sender_name'],
-			'company' => $this->config['sender_company'],
-			'street1' => $this->config['sender_street1'],
-			'city' => $this->config['sender_city'],
-			'state' => $this->config['sender_state'],
-			'zip' => $this->config['sender_zip'],
-			'country' => $this->config['sender_country'],
-			'phone' => $this->config['sender_phone'],
-			'email' => $this->config['sender_email']
-		);
-
-		$to_address = array(
-			'object_purpose' => 'PURCHASE',
-			'name' => $customer['first_name'] . " " . $customer['last_name'],
-			'street1' => $customer['shipping_address_1'],
-			'city' => $customer['shipping_city'],
-			'state' => $customer['shipping_state'],
-			'zip' => $customer['shipping_zip'],
-			'country' => $customer['shipping_country'],
-			'email' => $customer['email'],
-		);
-
-		// Parcel information array
-		$parcel = array(
-			'length'=> '3.25',
-			'width'=> '5.75',
-			'height'=> '1.75',
-			'distance_unit'=> 'in',
-			'weight'=> $total_weight,
-			'mass_unit'=> 'lb',
-		);
-
-		// Shipment object
-		$shipment = Shippo_Shipment::create(
-			array(
-				'object_purpose'=> 'PURCHASE',
-				'address_from'=> $from_address,
-				'address_to'=> $to_address,
-				'parcel'=> $parcel,
-				'async'=> false,
-			));
+		/**
+		* Get Rates from API
+		*
+		* Retrieve all available shipping rates from Shippo API
+		*
+		* @return array
+		**/
+    public function getRatesFromAPI()
+    {
+			$cart_items = $this->addon->api('bison')->getCartItems();
+			$shipment = $this->createShipment($cart_items);
 
 			// Rates are stored in the `rates_list` array inside the shipment object
 			$rates = $shipment['rates_list'];
@@ -105,5 +59,94 @@ class Core_bison_shippo_shipping extends Core
 			}
 
 			return $vars;
+    }
+
+
+		/**
+		 * Rate Details
+		 *
+		 * Gets a value from the active shipping option
+		 *
+		 * @return string
+		 */
+		public function rateDetails($key)
+		{
+			$rates = $this->getRates();
+			$customer = $this->addon->api('bison')->getCustomerInfo();
+
+			if ($customer['shipping_option'] != '') {
+				$option = $customer['shipping_option'];
+			} else {
+				$keys = array_keys($rates);
+				$option = $keys[0];
+			}
+
+			switch ($key) {
+				case 'value':
+					return $option;
+					break;
+				default:
+					return $rates[$option][$key];
+					break;
+			}
+
 		}
-	}
+
+		public function createShipment($items) {
+			Shippo::setApiKey($this->fetchConfig('api_key'));
+
+			$bison_config = $this->addon->api('bison')->getBisonConfig();
+			$total_weight = 0;
+			$customer = $this->addon->api('bison')->getCustomerInfo();
+
+			foreach ($items as $item) {
+				$total_weight += $item[$bison_config['weight_field']] * $item['quantity'];
+			}
+
+			$from_address = array(
+					'object_purpose' => 'PURCHASE',
+					'name' => $this->config['sender_name'],
+					'company' => $this->config['sender_company'],
+					'street1' => $this->config['sender_street1'],
+					'city' => $this->config['sender_city'],
+					'state' => $this->config['sender_state'],
+					'zip' => $this->config['sender_zip'],
+					'country' => $this->config['sender_country'],
+					'phone' => $this->config['sender_phone'],
+					'email' => $this->config['sender_email']
+			);
+
+			$to_address = array(
+					'object_purpose' => 'PURCHASE',
+					'name' => $customer['first_name'] . " " . $customer['last_name'],
+					'street1' => $customer['shipping_address_1'],
+					'city' => $customer['shipping_city'],
+					'state' => $customer['shipping_state'],
+					'zip' => $customer['shipping_zip'],
+					'country' => $customer['shipping_country'],
+					'email' => $customer['email'],
+			);
+
+			// Parcel information array
+			$parcel = array(
+					'length'=> '3.25',
+					'width'=> '5.75',
+					'height'=> '1.75',
+					'distance_unit'=> 'in',
+					'weight'=> $total_weight,
+					'mass_unit'=> 'lb',
+			);
+
+			// Shipment object
+			$shipment = Shippo_Shipment::create(
+			array(
+					'object_purpose'=> 'PURCHASE',
+					'address_from'=> $from_address,
+					'address_to'=> $to_address,
+					'parcel'=> $parcel,
+					'async'=> false,
+			));
+
+			return $shipment;
+		}
+}
